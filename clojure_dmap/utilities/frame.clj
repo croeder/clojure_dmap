205e8bf3baaca6f7d5a0250d0b400e53a7035977
Chris Roeder <croeder@croeder.com>
2014-03-24 07:07:18 -0600

    frames basicaly working

--- clojure_dmap/utilities/frame.clj
+++ clojure_dmap/utilities/frame.clj
@@ -9,10 +9,10 @@
 
 ;; TODO 
 ;; - abstractions and specializatons are linked lists, you only go up or down one level and then that frame tells you the next level up/down
-;;   - working on adding the abs when you add a spec, and vice-versa (see START HERE)
-;; ------------ not getting both, crashes
+;; - desperately need a testing framework
 ;; - switch from defstructs to the new thing...records
 ;; - features not slots
+;; - *** (nil) for specialization lists instead of ()
 
 (defstruct frame :name :features :specializations :abstractions)
 
@@ -37,17 +37,23 @@
 								))  ))
 				; specializations
 				(cond (frame-of frame-name)
-						(conj ((frame-of frame-name) :specializations (first stuff)))
+						(conj ((frame-of frame-name) :specializations 
+							;(first stuff)
+					 		(cond (empty? stuff) (list) :t (list (first stuff)))
+						))
 					:t 	(cond (empty? stuff) (list) :t (list (first stuff))))
 
 				; abstractions
 				(cond (frame-of frame-name)
-						(conj ((frame-of frame-name) :abstractions (second stuff)))
+						(conj ((frame-of frame-name) :abstractions 
+							;(second stuff)
+					 		(cond (empty? stuff) (list) :t (list (second stuff)))
+						))
 					:t 	(cond (empty? stuff) (list) :t (list (second stuff))))
 ))))
 
 (defn get-specializations [frame-name]
-	(:specializations (frame-of frame-name) :specializations))
+	(:specializations (frame-of frame-name) ))
 	;;(cond (not (nil? (frame-of frame-name))) ((frame-of frame-name) :specializations)))
 
 (defn get-abstractions [frame-name]
@@ -95,20 +101,14 @@
 (defn get-feature
 	"looks first in the native frame, then abstractions, then specializations, returning the first found"
  [frame-name feature-name]
-	(println "x"  (frame-name frame-of) )
 	(cond (feature-name (:features (frame-name frame-of)) )
-		; look local
-		 (feature-name (:features (frame-name frame-of))) 
-		:t
-		; look in abstractions and specializations
-		(and 
-			:t
-			;; for the list of frames in the specializations list, try to get the feature there recursively
-			(map (fn [fr-name]  (get-feature fr-name feature-name))
-				 	(get-specializations frame-name )) 
-			(map (fn [fr-name]  (get-feature fr-name feature-name))
-				 	(get-abstractions frame-name ))
-)))
+			(feature-name (:features (frame-name frame-of))) 
+		  :t
+			(and 
+				(map (fn [fr-name] 	(get-feature fr-name feature-name))
+				 	(get-specializations frame-name ))
+				(map (fn [fr-name]  (get-feature fr-name feature-name))
+			 		(get-abstractions frame-name ))  )))
 
 (defn print-frame [frame-name]
 	(cond (and 
@@ -123,41 +123,46 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-(print-frame nil)
-(create-frame :empty-person (list :fname :lname :mi)  nil  )
-(print-frame :empty-person)
-(println "empty person:")
-(print-frame :empty-person)
-(println "--------------")
+;;(print-frame nil)
+;;(create-frame :empty-person (list :fname :lname :mi)  nil  )
+;;(print-frame :empty-person)
+;;(println "empty person:")
+;;(print-frame :empty-person)
+;;(println "--------------")
 
-(create-frame :new-person (list :fname :lname :mi) (list "Bob" "Jones" "X") nil nil)
-(println "new person:")
-(print-frame :new-person)
-(println "last name of new person:")
-(println (base-slot :new-person :lname))
-(println "--------------")
+(create-frame :new-person (list :fname :lname :mi) (list "Bob" "Jones" "X") )
+;;(println "new person:")
+;;(print-frame :new-person)
+;;(println "last name of new person:")
+;;(println (base-slot :new-person :lname))
+;;(println "--------------")
 
 (create-frame :base-thing (list :dbid :mode) (list 123 :test))
 (create-frame :new-director (list :company :title :salary) (list "The" "Director" 1000000) :new-person :base-thing)
-(println "director with base-thing as abstraction, and new-person as specialization")
-(print-frame :new-director)
-(println "--------------")
-(println "specializations:" (get-specializations :new-director))
-(println "THIS IS NULL ?print-frame of specializations") (print-frame (first(get-specializations :new-director)))
-(print-frame :base-thing)
-(println "------------_________________--")
-(println "abstractions:" (get-abstractions :new-director))
-(println "print-frame of abstractions") (print-frame (first(get-abstractions :new-director)))
-(print-frame :new-person)
-(println "------------_________________--")
-(println "------------_________________--")
-(print-frame :new-director)
-(println "------------_________________--")
-(println "------------_________________--")
-(println "_________________")
-(println "title?:" (get-feature :new-director :title))
-(println "_________________")
+;;(println "director with base-thing as abstraction, and new-person as specialization")
+;;(print-frame :new-director)
+;;(println "--------------")
+;;(println "specializations:" (get-specializations :new-director))
+;;(println "THIS IS NULL ?print-frame of specializations") (print-frame (first(get-specializations :new-director)))
+;;(print-frame :base-thing)
+;;(println "------------_________________--")
+;;(println "abstractions:" (get-abstractions :new-director))
+;;(println "print-frame of abstractions") (print-frame (first(get-abstractions :new-director)))
+;;(print-frame :new-person)
+;;(println "------------_________________--")
+;;(println "------------_________________--")
+;;(print-frame :new-director)
+;;(println "------------_________________--")
+;;(println "------------_________________--")
 (println "lname?:" (get-feature :new-director :lname))
-(println "_________________")
+(println "title?:" (get-feature :new-director :title))
 (println "dbid?:" (get-feature :new-director :dbid))
-(println "_________________")
+;`;(println "------------_________________--")
+;(println "spec" (get-specializations :new-director))
+;(println "xx-----")
+(println  (map (fn [x]  (get-feature x :lname))
+	(get-specializations :new-director)))
+(println "-----xx")
+;(println "get frame ----\\/\\/\\/----")
+;(print-frame :new-director)
+
